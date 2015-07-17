cd43f97c24aa35eaf13e8eaebbba2934f7d3e4ec
tvinchent <tvinchent@gmail.com>
2015-07-17 13:15:14 +1200

    STILL WORKING - mapping finished

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ TO DO
 - faire un tableau de la requete: a partir d'un tableau sans valeur superflu, en précisant dans le insert le bon ordre
 
 Axe d'amelioration:
+- throw error quand le mapping ne reconnait rien, ne pas le mettre simplement dans la valeur de requette qui ne sera jamais visible
 - faire des unset commencant a 1 et pas a 0 pour supprimer le décallage
 - refactorisation simple: juste isoler les controles en fonction (pb: je parse a chaque fois le fichier donc essayer plutot de stocker mon parse dans une global)
 Pour cela: fonctionnement agile, cad commitable en partie fonctionelle
@@ -65,6 +66,20 @@ Refactorisation:
 - [fonction] prepare for BDD (singleton, use / herite de delete): si contenu du CSV ok // insérer ici en dur les colonnes a delete
 - [fonction] envoie BDD (use / herite de prepare for BDD)
 */
+
+$var1 = "variable1";
+$var2 = "variable3";
+$tableau = array($var1, $var2);
+ 
+switch ($tableau){
+    case array("variable1", "variable2"):
+        echo "Le tableau correspond !";
+    break;
+     
+    case array(NULL, NULL):
+        echo "Le tableau ne correspond pas.";
+    break; 
+}
 
 // include setting
 if (file_exists("include/setting_prod.php")) include_once 'include/setting_prod.php'; else include_once 'include/setting.php';
@@ -118,14 +133,15 @@ if(isset($_POST["submit"]) && isset($_FILES["csv"])){
                     // if I unset 29, I need to unset 28
     // PRODUCTION SPECIFIC CODE
                     unset($lines[0],$lines[1],$lines[2],$lines[3],$lines[4],$lines[5],$lines[6],$lines[7],$lines[8],$lines[9]);
+                    unset($lines[16]);
                     unset($lines[20],$lines[21],$lines[22],$lines[23],$lines[24],$lines[25],$lines[26],$lines[27],$lines[28]);
-                    unset($lines[39]);
+                    unset($lines[35],$lines[39]);
                     unset($lines[40],$lines[41],$lines[42],$lines[43],$lines[44],$lines[45],$lines[46],$lines[47]);
-                    unset($lines[58],$lines[59]);
+                    unset($lines[54],$lines[58],$lines[59]);
                     unset($lines[60],$lines[61],$lines[62],$lines[63],$lines[64],$lines[65],$lines[66],$lines[67],$lines[68],$lines[69]);
                     unset($lines[70],$lines[71],$lines[72],$lines[73],$lines[74],$lines[75],$lines[76],$lines[77],$lines[78],$lines[79]);
-                    unset($lines[80],$lines[81],$lines[82],$lines[83],$lines[84],$lines[85],$lines[86],$lines[87],$lines[86]);
-                    unset($lines[96]);
+                    unset($lines[80],$lines[81],$lines[82],$lines[83],$lines[84],$lines[85]);
+                    unset($lines[92],$lines[96]);
     // END OF PRODUCTION SPECIFIC CODE
                     // END OF SPECIFIC LINES DELETION
                     // parse every lines
@@ -136,12 +152,79 @@ if(isset($_POST["submit"]) && isset($_FILES["csv"])){
 // (prendre juste le else, if pas pour le moment car le inception simple ne sert que pour le graphique et c'est pas clair // if column != 87 à 96 supprimer tout element inutile dont inception simple else supprimer tout element inutile mais sans inception simple
 // supprimer tout element inutile dont fund et inception simple
 // enlever le % (seulement si )
+                        $value_for_db_insert="'";
+                // START OF SPECIFIC INSERT
+// if column = x alors fund = act/bal/cons en mappant dans les parentheses apres le $table les bonnes colonnes
+// & colmunsOrderForInsert = 
+// & table = perf_after_tax
+// if column = x alors fund = act/bal/cons en mappant dans les parentheses apres le $table les bonnes colonnes
+// & table = perf_table
+// test: echo $insertDB
+                        switch ($column[0]) {
+                            case '700200':
+                                $value_for_db_insert.='act';
+                                break;
+                            case '700201':
+                                $value_for_db_insert.='dyn';
+                                break;
+                            case '700202':
+                                $value_for_db_insert.='bal';
+                                break;
+                            case '700203':
+                                $value_for_db_insert.='tas';
+                                break;
+                            case '700204':
+                                $value_for_db_insert.='glob';
+                                break;
+                            case '700205':
+                                $value_for_db_insert.='inc';
+                                break;
+                            case '700300':
+                                $value_for_db_insert.='x_act';
+                                break;
+                            case '700301':
+                                $value_for_db_insert.='x_bal';
+                                break;
+                            case '700302':
+                                $value_for_db_insert.='cons';
+                                break;
+
+                            default:
+                                $value_for_db_insert.='error fund';
+                                break;
+                        }
+                        switch ($column[2]) {
+                            case 'High PIR Return':
+                                $value_for_db_insert.='_2800';
+                                $table_for_db_insert=$dbTable2;
+                                break;
+                            case 'Medium PIR Return':
+                                $value_for_db_insert.='_1750';
+                                $table_for_db_insert=$dbTable2;
+                                break;
+                            case 'Low PIR Return':
+                                $value_for_db_insert.='_1050';
+                                $table_for_db_insert=$dbTable2;
+                                break;
+                            case 'Zero PIR Return':
+                                $table_for_db_insert=$dbTable1;
+                                break;
+                            
+                            default:
+                                $value_for_db_insert.='error return type';
+                                $table_for_db_insert='error table';
+                                break;
+                        }
+                        $colmuns_order_for_db_insert='fund,since_inception,1_month,3_months,6_months,1_year,2_years,3_years,5_years';
+                        $value_for_db_insert.='\',\'';
+                // END OF SPECIFIC INSERT
+
                         unset($column[0],$column[1],$column[2],$column[3],$column[9]);
+// since inception (juste for graph): set 
                         unset($column[11],$column[13],$column[15],$column[16],$column[17],$column[18],$column[19]);
                         unset($column[20],$column[21],$column[22]);
 // END OF SPECIFIC COLUMN DELETION
-                            $value_for_db_insert="'";
-                            foreach ($column as $key=>$element) {
+                        foreach ($column as $key=>$element) {
                                 // parse every columns
 // START OF SPECIFIC VALUE FORMAT
                             $caseWithJustASpace = "\r";
@@ -156,21 +239,12 @@ if(isset($_POST["submit"]) && isset($_FILES["csv"])){
                             }
 // END OF SPECIFIC VALUE FORMAT
                                 $value_for_db_insert.=$element."', '";
-                            }
-    // !!!!!!!!!!!!! A DEPLACER DANS UNE FONCTION DEDIE A L'INSERTION
-                            // remove last comma
-                            $value_for_db_insert = substr($value_for_db_insert,0,strlen($value_for_db_insert)-3);
-                            // then insert
-                    // START OF SPECIFIC INSERT
-// if column = x alors fund = act/bal/cons en mappant dans les parentheses apres le $table les bonnes colonnes
-// & table = perf_after_tax
-// if column = x alors fund = act/bal/cons en mappant dans les parentheses apres le $table les bonnes colonnes
-// & table = perf_table
-// test: echo $insertDB
-                    // END OF SPECIFIC INSERT
-                            //WIP $result = $db->exec("INSERT INTO $Table VALUES(".$value_for_db_insert.")");
-                            echo 'value: <br>';
-                            var_dump($value_for_db_insert);
+                        }
+// !!!!!!!!!!!!! A DEPLACER DANS UNE FONCTION DEDIE A L'INSERTION
+                        // remove last comma
+                        $value_for_db_insert = substr($value_for_db_insert,0,strlen($value_for_db_insert)-3);
+                        // then insert
+                        $result = $db->exec("INSERT INTO $table_for_db_insert ($colmuns_order_for_db_insert) VALUES($value_for_db_insert)");
 
     // !!!!!!!!!!!!! END OF A DEPLACER DANS UNE FONCTION DEDIE A L'INSERTION
                     }
@@ -218,7 +292,9 @@ if(isset($_POST["submit"]) && isset($_FILES["csv"])){
 <input type="file" name="csv"></input>
 <input type="submit" name="submit" value="Upload file"></input>
 </form>
-<p>Note: To resolve a malformation of your CVS, it must be at the Windows format. Open it in Excel and save it as a "<strong>Windows</strong> comma separated value"</p>
+<p>Note: To resolve a malformation of your CVS, it must be at the Windows format. Open it in Excel and save it as a "<strong>Windows</strong> comma separated value"
+<br>Do no hesitate to make a copy of your 2 table database before using.
+</p>
 
 </body>
 </html>
